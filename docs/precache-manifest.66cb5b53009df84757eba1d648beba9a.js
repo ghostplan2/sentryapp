a3a71ff4810ed6c32df4ef0108c517866c7fe6e1
ghostplan2 <ghost_plan_2@mail.com>
2019-12-06 21:04:35 +0400

    docs

---
+++ docs/precache-manifest.66cb5b53009df84757eba1d648beba9a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a640bc9a1344809ef0f14d472914bbdd",
+    "url": "/sentry/index.html"
+  },
+  {
+    "revision": "6e7e50ec932ee181de03",
+    "url": "/sentry/static/css/main.5df7d988.css"
+  },
+  {
+    "revision": "6e7e50ec932ee181de03",
+    "url": "/sentry/static/js/main.40c9eb04.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sentry/static/js/main.40c9eb04.js.LICENSE"
+  }
+]);
